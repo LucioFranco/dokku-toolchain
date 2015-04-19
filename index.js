76a252bac6e660197eb7542e402cb0085ce2265f
Lucio Franco <lucio.franco@du.edu>
2015-04-19 17:09:52 -0600

    added no pass auth

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ var app = require('commander'),
     util = require('util'),
     fs = require('fs'),
     pkg = require('./package.json'),
+    prompt= require('prompt'),
     exec = require('child_process').exec;
 
 app
   .version(pkg.version);
+
+// removing the default prompt message and delimeter  
+prompt.message = '';
+prompt.delimiter = '';
 
 // add <appname> command
 // allows user to add git remote name dokku for their server
@@ -113,13 +118,12 @@ app
 // allows user to setup their dokku server with dok
 // places information into config.json
 app
-  .command('set <hostname> <username> <password>')
+  .command('set <hostname> <username>')
   .description('sets the hostname, user, and password for the dokku server')
-  .action(function (hostname, username, password) {
+  .action(function (hostname, username) {
     var server = {
       host: hostname,
-      user: username,
-      pass: password
+      user: username
     };
 
     jf.writeFile('config.json', server, function (err) {
@@ -144,22 +148,36 @@ app
         console.log('Please run dok set <hostname> <username> <password>');
         process.exit(1);
       }else {
-        console.log('accessing the server.....');
-        var shell = new SSH({
-          host: file.host,
-          user: file.user,
-          pass: file.pass
-        });
+        prompt.start();
+        prompt.get({
+            properties: {
+              password: {
+                hidden: true,
+                required: true
+              }
+            }
+          }, function (err, res) {
+            if(err) {
+              console.err(err);
+            }else {
+              console.log('accessing the server.....');
+              var shell = new SSH({
+                host: file.host,
+                user: file.user,
+                pass: res.password
+              });
 
-        shell.exec('sudo dokku ' + app.args.join(' '), {
-          pty: true,
-          out: function (stdout) {
-            console.log(stdout);
-          },
-          err: function (stderr) {
-            console.log(stderr);
-          }
-        }).start();
+              shell.exec('sudo dokku ' + app.args.join(' '), {
+                pty: true,
+                out: function (stdout) {
+                  console.log(stdout);
+                },
+                err: function (stderr) {
+                  console.log(stderr);
+                }
+              }).start();
+            }
+        });
       }
     });
   });
